7148cd8a630c3170f2a105e97e2eeee2551edbf4
Nimblebody <62288670+Nimblebody@users.noreply.github.com>
2023-04-27 15:20:48 +0900

    코드연 습밑 테스트 제작

--- Assets/Scripst/test/PlayerTest.cs
+++ Assets/Scripst/test/PlayerTest.cs
@@ -4,26 +4,55 @@ using UnityEngine;
 public class PlayerTest : MonoBehaviour
 {
     [SerializeField] private float walkSpeed;
+    [SerializeField] private float RunSpeed;
+    [SerializeField] private float ApplySpeed;
+    private bool isRun = false;
     [SerializeField] private float lookSensitivity;
     [SerializeField] private float cameraRtatLimit;
     [SerializeField] private float currentCameraRotationX = 0;
-    
+
     [SerializeField] private Camera theCamera;
-    private Rigidbody myRigid; 
+    private Rigidbody myRigid;
     void Start()
     {
         theCamera = FindObjectOfType<Camera>();
-        myRigid = GetComponent<Rigidbody>();    
+        myRigid = GetComponent<Rigidbody>();
+        ApplySpeed = walkSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
+        TryRun();
         Move();
         CharacterRotation();
         CameraRotation();
     }
 
+    public void TryRun()
+    {
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            Running();
+        }
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            RunningCancel();
+        }
+    }
+
+    public void Running()
+    {
+        isRun = true;
+        ApplySpeed = RunSpeed;
+    }
+
+    public void RunningCancel()
+    {
+        isRun = false;
+        ApplySpeed = walkSpeed;
+    }
+    
     public void Move()
     {
         float _moveDirx = Input.GetAxisRaw("Horizontal");
@@ -32,7 +61,7 @@ public class PlayerTest : MonoBehaviour
         Vector3 _moveHorizontal = transform.right* _moveDirx;   
         Vector3 _moveVertical = transform.forward* _moveDirz;
 
-        Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * walkSpeed;
+        Vector3 _velocity = (_moveHorizontal + _moveVertical).normalized * ApplySpeed;
 
         myRigid.MovePosition(transform.position + _velocity * Time.deltaTime);
 
